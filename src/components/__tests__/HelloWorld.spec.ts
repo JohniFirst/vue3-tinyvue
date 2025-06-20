0dd8a839095d3d675823efe36bd11000859b3016
JohniFirst <a15708295047@outlook.com>
2025-06-20 14:19:28 +0800

    修改测试案例

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import HelloWorld from '@/views/home-view.vue'
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+    const wrapper = mount(HelloWorld)
+    expect(wrapper.text()).toContain('Hello Vue')
   })
 })
