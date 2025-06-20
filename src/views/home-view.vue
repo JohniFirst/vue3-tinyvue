3119a71084ae5e307940dbc6df39ad583f9f440b
JohniFirst <a15708295047@outlook.com>
2025-06-20 14:16:10 +0800

    init

--- src/views/home-view.vue
+++ src/views/home-view.vue
@@ -2,4 +2,10 @@
   <h1>Hello Vue</h1>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  console.log('Home view mounted');
+});
+</script>
