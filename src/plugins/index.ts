6e140abd3504b8d81f5da41ced89a1ec24c34934
Johnifirst <a15708295047@outlook.com>
2025-07-14 22:50:19 +0800

    chore: 删除插件统一导出文件，简化项目结构

--- src/plugins/index.ts
+++
@@ -1,5 +0,0 @@
-// 插件统一导出文件
-export { default as httpPlugin } from './http-plugin'
-export { default as authPlugin } from './auth-plugin'
-export { default as utilsPlugin } from './utils-plugin'
-export { default as directivePlugin } from './directive-plugin'
